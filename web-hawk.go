40bb3108b3b24651526de2f900813eae92c6eb99
Ian Chan <icha024@gmail.com>
2016-07-01 23:56:48 +0100

    Polishing

--- web-hawk.go
+++ web-hawk.go
@@ -17,8 +17,7 @@ func main() {
 		Time  float64
 	}
 	portPtr := flag.String("port", "8080", "Port to host location service on.")
-	// urlsPtr := flag.String("urls", "http://localhost:8080/up,http://www.clianz.com/", "Comma seperated URLs list to monitor")
-	urlsPtr := flag.String("urls", "http://localhost:8080/up", "Comma seperated URLs list to monitor")
+	urlsPtr := flag.String("urls", "http://localhost:8080/up,http://www.clianz.com/", "Comma seperated URLs list to monitor")
 	flag.Parse()
 	if os.Getenv("PORT") != "" {
 		*portPtr = os.Getenv("PORT")
@@ -34,7 +33,7 @@ func main() {
 		Timeout: timeout,
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/up", func(w http.ResponseWriter, r *http.Request) {
 		queue := make(chan serviceStats, len(urls))
 		for _, eachURL := range urls {
 			go func(v string) {
@@ -46,7 +45,7 @@ func main() {
 					queue <- serviceStats{Alive: false, URL: v, Time: 0}
 				} else {
 					endTime := time.Since(startTime).Seconds() * 1000
-					log.Printf("Success. Duration: %v", endTime)
+					log.Printf("Success. Duration: %f", endTime)
 					queue <- serviceStats{Alive: true, URL: v, Time: endTime}
 				}
 			}(eachURL)
@@ -57,11 +56,10 @@ func main() {
 		for i := 0; i < len(urls); i++ {
 			select {
 			case elem := <-queue:
-				log.Printf("Alive: %v, Time: %f, URL: %v", elem.Alive, elem.Time, elem.URL)
 				if i != 0 {
 					fmt.Fprintf(w, ", ")
 				}
-				fmt.Fprintf(w, "alive: %v, time: %v, url: \"%v\"", elem.Alive, elem.Time, elem.URL)
+				fmt.Fprintf(w, "alive:%v, msec:%.2f, url:\"%v\"", elem.Alive, elem.Time, elem.URL)
 			}
 		}
 		close(queue)
